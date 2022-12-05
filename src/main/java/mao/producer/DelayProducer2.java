ad4ba788adc32afe89197008125dd089a9480b28
mao <1296193245@qq.com>
2022-12-05 21:27:09 +0800

    RocketMQ_delay_messages_send_and_receive

--- src/main/java/mao/producer/DelayProducer2.java
+++ src/main/java/mao/producer/DelayProducer2.java
@@ -13,22 +13,38 @@ import java.util.Date;
 /**
  * Project name(项目名称)：RocketMQ_延迟消息的发送与接收
  * Package(包名): mao.producer
- * Class(类名): DelayProducer
+ * Class(类名): DelayProducer2
  * Author(作者）: mao
  * Author QQ：1296193245
  * GitHub：https://github.com/maomao124/
  * Date(创建日期)： 2022/12/5
- * Time(创建时间)： 20:52
+ * Time(创建时间)： 21:11
  * Version(版本): 1.0
- * Description(描述)： 生产者-延迟消息
+ * Description(描述)： 无
  */
 
-public class DelayProducer
+public class DelayProducer2
 {
     /**
      * 简单日期格式
      */
     static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    /**
+     * 得到int随机
+     *
+     * @param min 最小值
+     * @param max 最大值
+     * @return int
+     */
+    public static int getIntRandom(int min, int max)
+    {
+        if (min > max)
+        {
+            min = max;
+        }
+        return min + (int) (Math.random() * (max - min + 1));
+    }
 
     public static void main(String[] args)
             throws MQClientException, MQBrokerException, RemotingException, InterruptedException
@@ -46,12 +62,14 @@ public class DelayProducer
             String format = simpleDateFormat.format(new Date());
             //消息对象
             Message message = new Message("test_topic", (format + " --> 消息" + i).getBytes(StandardCharsets.UTF_8));
-            //设置延时，水平4为30秒
-            message.setDelayTimeLevel(4);
+            //设置随机延时
+            //int level = getIntRandom(1, 4);
+            int level = i < 50 ? 4 : 2;
+            message.setDelayTimeLevel(level);
             //发送消息
             defaultMQProducer.send(message);
             //打印
-            System.out.println(format + " --> 消息" + i);
+            System.out.println(format + " --> 消息" + i + "    延迟水平：" + level);
         }
         //关闭
         defaultMQProducer.shutdown();
